f8bb5d98d60cfd1509abff662e623af638e45201
AlDagor <milya89@gmail.com>
2020-01-14 20:49:13 +0200

    minor corrects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Quiz from '../../containers/Quiz/Quiz';
 import Layout from '../../hoc/Layout/Layout';
-import { App } from './App.styled';
 
 export default () => (
   <Layout>
--- src/components/App/App.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const App = styled.div`
-  color: #555;
-  text-align: center;
-`;
