3ca68ba50bb7f4336b9f16a7dfb277403786ca31
Alexander Miliutin <milya89@gmail.com>
2020-07-18 22:27:43 +0300

    add auth validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Button from '../../components/Button/Button';
-import Input from '../../components/Input/Input';
-import { Auth, Container, Title, Form } from './Auth.styled';
+import React, { useState } from 'react'
+import is from 'is_js'
+import Button from '../../components/Button/Button'
+import Input from '../../components/Input/Input'
+import { Auth, Container, Title, Form } from './Auth.styled'
 
 export default () => {
   const [formControls, setFormControls] = useState({
@@ -13,7 +14,7 @@ export default () => {
       valid: false,
       touched: false,
       validation: {
-        requierd: true,
+        required: true,
         email: true
       }
     },
@@ -25,11 +26,12 @@ export default () => {
       valid: false,
       touched: false,
       validation: {
-        requierd: true,
+        required: true,
         minLength: 6
       }
     }
   });
+  const [isFormValid, setFormValid] = useState(false)
 
   const loginHandler = () => {
     console.log('login')
@@ -40,16 +42,54 @@ export default () => {
   }
 
   const submitHandler = e => {
-    e.preventDefault();
+    e.preventDefault()
+  }
+
+  const validateControl = (value, validation) => {
+    if (!validation) {
+      return true
+    }
+
+    let isValid = true
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (validation.email) {
+      isValid = is.email(value) && isValid
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid
   }
 
   const onChangeHandler = (e, controlName) => {
-    console.log(`${controlName}: ${e.target.value}`);
+    const newFormControls = { ...formControls }
+    const control = { ...newFormControls[controlName] }
+
+    control.value = e.target.value
+    control.touched = true
+    control.valid = validateControl(control.value, control.validation)
+
+    newFormControls[controlName] = control
+
+    let isFormValid = true
+    Object.keys(newFormControls).forEach(name => {
+      isFormValid = newFormControls[name].valid && isFormValid
+    })
+
+    setFormValid(isFormValid)
+    setFormControls(newFormControls)
   }
 
   const renderInputs = () => Object.keys(formControls).map((controlName, index) => {
     const control = formControls[controlName];
     const { value, type, label, errorMessage, valid, touched, validation } = control;
+
     return (
       <Input
         key={controlName + index}
@@ -73,8 +113,21 @@ export default () => {
         <Form onSubmit={submitHandler}>
           {renderInputs()}
 
-          <Button type='success' style={{marginRight: '20px'}} onClick={loginHandler}>Войти</Button>
-          <Button type='primary' onClick={registerHandler}>Зарегистрироваться</Button>
+          <Button
+            type='success'
+            style={{ marginRight: '20px' }}
+            onClick={loginHandler}
+            disabled={!isFormValid}
+          >
+            Войти
+          </Button>
+          <Button
+            type='primary'
+            onClick={registerHandler}
+            disabled={!isFormValid}
+          >
+            Зарегистрироваться
+          </Button>
         </Form>
       </Container>
     </Auth>
