7bad96ad361129a017bbe97a81b6b1403935d0a4
Alexander Miliutin <milya89@gmail.com>
2020-07-27 20:19:14 +0300

    add QuizCreator

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,9 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Button from '../../components/Button/Button'
+import Input from '../../components/Input/Input'
+import Select from '../../components/Select/Select'
+import { createControl, validate, validateForm } from '../../form/formFramework'
+import { QuizCreator, Wrapper, Title, Form } from './QuizCreator.styled'
+
+const createOptionControl = number => createControl({
+  label: `Вариант ${number}`,
+  errorMessage: 'Значение не может быть пустым',
+  id: number
+}, { required: true })
+
+const createFormControls = () => ({
+  question: createControl({
+    label: 'Введите вопрос',
+    errorMessage: 'Вопрос не может быть пустым'
+  }, { required: true }),
+  option1: createOptionControl(1),
+  option2: createOptionControl(2),
+  option3: createOptionControl(3),
+  option4: createOptionControl(4),
+})
 
 export default () => {
+  const [quiz, setQuiz] = useState([])
+  const [formControls, setFormControls] = useState(createFormControls())
+  const [rightAnswerId, setRightAnswerId] = useState(1)
+  const [isFormValid, setIsFormValid] = useState(false)
+
+  const submitHandler = e => {
+    e.preventDefault()
+  }
+
+  const addQuestionHandler = e => {
+    e.preventDefault()
+
+    const newQuiz = quiz.concat()
+    const index = newQuiz.length + 1
+    const { question, option1, option2, option3, option4 } = formControls
+
+    const questionItem = {
+      question: question.value,
+      id: index,
+      rightAnswerId,
+      answers: [
+        { text: option1.value, id: option1.id },
+        { text: option2.value, id: option2.id },
+        { text: option3.value, id: option3.id },
+        { text: option4.value, id: option4.id }
+      ]
+    }
+
+    newQuiz.push(questionItem)
+    setQuiz(newQuiz)
+    setFormControls(createFormControls())
+    setRightAnswerId(1)
+    setIsFormValid(false)
+  }
+
+  const createQuizHandler = e => {
+    e.preventDefault()
+  }
+
+  const changeHandler = (value, controlName) => {
+    const newFormControls = { ...formControls }
+    const control = { ...newFormControls[controlName] }
+
+    control.touched = true
+    control.value = value
+    control.valid = validate(control.value, control.validation)
+
+    newFormControls[controlName] = control
+
+    setFormControls(newFormControls)
+    setIsFormValid(validateForm(newFormControls))
+  }
+
+  const selectChangeHandler = e => {
+    setRightAnswerId(+e.target.value)
+  }
+
+  const renderControls = () => {
+    return Object.keys(formControls).map((controlName, index) => {
+      const { label, value, valid, validation, touched, errorMessage } = formControls[controlName]
+
+      return (
+        <div key={controlName + index}>
+          <Input
+            label={label}
+            value={value}
+            valid={valid}
+            shouldValidate={!!validation}
+            touched={touched}
+            errorMessage={errorMessage}
+            onChange={e => changeHandler(e.target.value, controlName)}
+          />
+          {index === 0 && <hr />}
+        </div>
+      )
+    })
+  }
+
+  const select = <Select
+    label='Выберите правильный ответ'
+    value={rightAnswerId}
+    onChange={selectChangeHandler}
+    options={[
+      { text: 1, value: 1 },
+      { text: 2, value: 2 },
+      { text: 3, value: 3 },
+      { text: 4, value: 4 }
+    ]}
+  />
+
   return (
-    <div>
-      QuizCreator
-    </div>
+    <QuizCreator>
+      <Wrapper>
+        <Title>Создание теста</Title>
+        <Form onSubmit={submitHandler}>
+          { renderControls() }
+          { select }
+
+          <Button
+            type='primary'
+            onClick={addQuestionHandler}
+            style={{ marginRight: '20px' }}
+            disabled={!isFormValid}
+          >
+            Добавить вопрос
+          </Button>
+
+          <Button
+            type='success'
+            onClick={createQuizHandler}
+            disabled={quiz.length === 0}
+          >
+            Создать тест
+          </Button>
+
+        </Form>
+      </Wrapper>
+    </QuizCreator>
   )
 }
---
+++ src/containers/QuizCreator/QuizCreator.styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const QuizCreator = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+  flex-grow: 1;
+  width: 100%;
+  background-image: linear-gradient(270deg, #f0613c, #f0783c);
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 600px;
+  padding: 0 20px;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  color: white;
+  margin-bottom: 20px;
+`;
+
+export const Form = styled.form`
+  background: #eee;
+  border-radius: 5px;
+  padding: 15px;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, .5);
+`;
